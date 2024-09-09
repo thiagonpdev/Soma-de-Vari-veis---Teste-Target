ece489c52e29baa8639fd5d4709e482ff69a593c
Thiago Nogueira <164404698+thiagonpdev@users.noreply.github.com>
2024-09-09 17:49:35 -0300

    Adicionar arquivos de projeto.

---
+++ Soma de Variáveis/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Soma_de_Variáveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int INDICE = 12;
+            int SOMA = 0;
+            int K = 1;
+
+            while (K < INDICE)
+            {
+                K = K + 1;
+                SOMA = SOMA + K;
+            }
+            Console.WriteLine(SOMA);
+        }
+    }
+}
